bf4830c228a80371019604f8c43614ef8aac808f
4zv4l <4zv4l@protonmail.com>
2024-08-07 12:37:04 +0800

    up: improve perl client

--- perl/zfs_client.pl
+++ perl/zfs_client.pl
@@ -4,40 +4,49 @@ use Digest::MD5;
 use IO::Socket::INET;
 use v5.38;
 
-# connect and send file request to server
-my $conn = IO::Socket::INET->new("127.0.0.1:9999") or die;
-$conn->send(scalar <>);
+$|=1;
 
-# get the md5sum or error if the hash is 0
-my $md5sum;
-$conn->recv($md5sum, 16);
-$md5sum = unpack("H*", $md5sum);
-say "md5sum => '$md5sum'";
+sub download($conn) {
+    print "> ";
+    my $filename = scalar <STDIN>;
+    $conn->send($filename);
+    chomp($filename);
 
-if ($md5sum eq '00000000000000000000000000000000')
-{
-    my $err;
-    $conn->recv($err, 100);
-    say $err;
-    exit;
+    # get md5sum
+    $conn->recv(my $md5sum, 16);
+    $md5sum = unpack("H*", $md5sum);
+    say "md5sum => '$md5sum'";
+    
+    # get filesize
+    $conn->recv(my $filesize, 8);
+    $filesize = unpack("Q", $filesize);
+    say "filesize => $filesize";
+    
+    if ($md5sum eq '00000000000000000000000000000000')
+    {
+        $conn->recv(my $err, $filesize);
+        say $err;
+        return;
+    }
+    
+    # download file and check md5sum
+    my $md5 = Digest::MD5->new;
+    my $downloaded = 0;
+    open my $output, '>', $filename;
+    while ($downloaded < $filesize)
+    {
+        $conn->recv(my $chunk, 1024); 
+        $md5->add($chunk);
+        $downloaded += length($chunk);
+        print "\rDownloaded $downloaded/$filesize bytes";
+    }
+    print "\n";
+    close($output);
+    
+    $md5sum eq $md5->hexdigest ? say "md5sum matches !" : say "md5sum does not match got '", $md5->hexdigest, "' !!!";
 }
 
-# get filesize
-my $filesize;
-$conn->recv($filesize, 8);
-$filesize = unpack("Q", $filesize);
-say "filesize => $filesize";
-
-# download file and check md5sum
-my $md5 = Digest::MD5->new;
-open my $output, '>', 'out';
-while (my $chunk = <$conn>)
-{
-    print $output $chunk;
-    $md5->add($chunk);
-}
-close($output);
-
-$md5sum eq $md5->hexdigest ? say "md5sum matches !" : say "md5sum does not match !!!";
-
+(my $ip, my $port) = ($ARGV[0] || "127.0.0.1", $ARGV[1] || 8080);
+my $conn = IO::Socket::INET->new("$ip:$port") or die;
+while(1) {download($conn)};
 $conn->close;
