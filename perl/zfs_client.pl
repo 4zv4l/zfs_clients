fbd5fa4ef1550d9b3a032ce2c3b280ac389ffc5d
4zv4l <4zv4l@protonmail.com>
2024-08-09 19:12:00 +0800

    feat: support commands

--- perl/zfs_client.pl
+++ perl/zfs_client.pl
@@ -6,43 +6,63 @@ use v5.38;
 
 $|=1;
 
+sub getMd5($conn) {
+    # get md5sum
+    $conn->recv(my $md5sum, 16);
+    $md5sum = unpack("H*", $md5sum);
+    say "md5sum => '$md5sum'";
+    $md5sum;
+}
+
+sub getSize($conn) {
+    # get filesize
+    $conn->recv(my $filesize, 8);
+    $filesize = unpack("Q", $filesize);
+    say "filesize => $filesize";
+    $filesize; 
+}
+
+sub getCmd($conn) {
+
+}
+
 sub download($conn) {
     print "> ";
     my $filename = scalar <STDIN>;
     $conn->send($filename);
     chomp($filename);
 
-    # get md5sum
-    $conn->recv(my $md5sum, 16);
-    $md5sum = unpack("H*", $md5sum);
-    say "md5sum => '$md5sum'";
-    
-    # get filesize
-    $conn->recv(my $filesize, 8);
-    $filesize = unpack("Q", $filesize);
-    say "filesize => $filesize";
-    
+    my $md5sum = getMd5($conn);
+    my $filesize = getSize($conn);
     if ($md5sum eq '00000000000000000000000000000000')
     {
         $conn->recv(my $err, $filesize);
         say $err;
         return;
     }
-    
-    # download file and check md5sum
-    my $md5 = Digest::MD5->new;
-    my $downloaded = 0;
-    open my $output, '>', $filename;
-    while ($downloaded < $filesize)
-    {
-        $conn->recv(my $chunk, 1024); 
-        $md5->add($chunk);
-        $downloaded += length($chunk);
-        print "\rDownloaded $downloaded/$filesize bytes";
+
+    my $md5 = Digest::MD5->new;;
+    if ($filename =~ /^\$/) {
+        $conn->recv(my $list, $filesize);
+        print "------ LIST ------\n";
+        print $list;
+        $md5->add($list);
+        print "------------------\n";
+    } else {
+        # download file and check md5sum
+        my $downloaded = 0;
+        open my $output, '>', $filename;
+        while ($downloaded < $filesize)
+        {
+            $conn->recv(my $chunk, 1024); 
+            $md5->add($chunk);
+            $downloaded += length($chunk);
+            print $output $chunk;
+            print "\rDownloaded $downloaded/$filesize bytes";
+        }
+        print "\n";
+        close($output);
     }
-    print "\n";
-    close($output);
-    
     $md5sum eq $md5->hexdigest ? say "md5sum matches !" : say "md5sum does not match got '", $md5->hexdigest, "' !!!";
 }
 
